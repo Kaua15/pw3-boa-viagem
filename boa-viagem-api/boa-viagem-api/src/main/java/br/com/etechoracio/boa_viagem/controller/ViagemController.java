de6764be69df7abffbb63c31acfff0ad9b2ac363
Kaua15 <kaualimadesouza15@gmail.com>
2022-05-02 08:48:33 -0300

    Atividade Avaliativa CRUD

--- boa-viagem-api/boa-viagem-api/src/main/java/br/com/etechoracio/boa_viagem/controller/ViagemController.java
+++ boa-viagem-api/boa-viagem-api/src/main/java/br/com/etechoracio/boa_viagem/controller/ViagemController.java
@@ -1,9 +1,10 @@
 package br.com.etechoracio.boa_viagem.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.data.RepositoryType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.StreamingHttpOutputMessage.Body;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,29 +15,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.etechoracio.boa_viagem.entity.Gasto;
+import br.com.etechoracio.boa_viagem.entity.Viagem;
 import br.com.etechoracio.boa_viagem.repository.GastoRepository;
-import java.util.List;
-import java.util.Optional;
-
-import javax.xml.ws.Response;
+import br.com.etechoracio.boa_viagem.repository.ViagemRepository;
 
 @RestController
-@RequestMapping("/gastos")
-public class GastoController {
+@RequestMapping("/viagem")
+public class ViagemController {
+
 	@Autowired
-	private GastoRepository repository;
+	private ViagemRepository repository;
 	
 	@GetMapping
-	public List<Gasto> listarTodos()
+	public List<Viagem> listarTodos()
 	{
 		return repository.findAll();
 
 	}
+	
 	//fazendo metodo de requisição da tabela gasto por id
 	@GetMapping("/{id}")
-	public ResponseEntity<Gasto> buscarPorID(@PathVariable Long id) 
+	public ResponseEntity<Viagem> buscarPorID(@PathVariable Long id) 
 	{
-		Optional<Gasto> existe = repository.findById(id);
+		Optional<Viagem> existe = repository.findById(id);
 		if(existe.isPresent())
 		{
 			return ResponseEntity.ok(existe.get());
@@ -47,31 +48,15 @@ public class GastoController {
 		}
 	}
 	
-	@DeleteMapping("/{id}")
-	public ResponseEntity<Object> excluir(@PathVariable Long id) 
-	{
-		boolean existe = repository.existsById(id);
-		
-		if(existe) 
-		{
-			repository.deleteById(id);
-			return ResponseEntity.ok().build();
-		} 
-		else
-		{
-			return ResponseEntity.notFound().build();
-		}
-	}
-	
 	@PostMapping
-	public ResponseEntity<Gasto> inserir(@RequestBody Gasto obj)
+	public ResponseEntity<Viagem> inserir(@RequestBody Viagem obj)
 	{
 		repository.save(obj);
 		return ResponseEntity.ok(obj);
 	}
 	
 	@PutMapping("/{id}")
-	public ResponseEntity<Gasto> atualizar(@PathVariable Long id, @RequestBody Gasto gasto) 
+	public ResponseEntity<Viagem> atualizar(@PathVariable Long id, @RequestBody Viagem viagem) 
 	{
 		boolean existe = repository.existsById(id);
 		
@@ -80,7 +65,7 @@ public class GastoController {
 			return ResponseEntity.notFound().build(); 
 		}
 		
-		repository.save(gasto);
-		return ResponseEntity.ok(gasto);
+		repository.save(viagem);
+		return ResponseEntity.ok(viagem);
 	}
 }
