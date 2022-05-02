2790639e8175db1b8918d19a47ab788d1eec4487
Kaua15 <kaualimadesouza15@gmail.com>
2022-05-02 07:41:11 -0300

    2

--- boa-viagem-api/boa-viagem-api/src/main/java/br/com/etechoracio/boa_viagem/controller/GastoController.java
+++ boa-viagem-api/boa-viagem-api/src/main/java/br/com/etechoracio/boa_viagem/controller/GastoController.java
@@ -1,15 +1,22 @@
 package br.com.etechoracio.boa_viagem.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.StreamingHttpOutputMessage.Body;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.etechoracio.boa_viagem.entity.Gasto;
 import br.com.etechoracio.boa_viagem.repository.GastoRepository;
 import java.util.List;
+import java.util.Optional;
+
+import javax.xml.ws.Response;
 
 @RestController
 @RequestMapping("/gastos")
@@ -25,14 +32,39 @@ public class GastoController {
 	}
 	//fazendo metodo de requisição da tabela gasto por id
 	@GetMapping("/{id}")
-	public Gasto buscarPorID(@PathVariable Long id) 
+	public ResponseEntity<Gasto> buscarPorID(@PathVariable Long id) 
 	{
-		return repository.findById(id).orElse(null);
+		Optional<Gasto> existe = repository.findById(id);
+		if(existe.isPresent())
+		{
+			return ResponseEntity.ok(existe.get());
+		}
+		else
+		{
+			return ResponseEntity.notFound().build();
+		}
 	}
 	
 	@DeleteMapping("/{id}")
-	public void deletarPorID(@PathVariable Long id) 
+	public ResponseEntity<Object> excluir(@PathVariable Long id) 
 	{
-		repository.findById(id);
+		boolean existe = repository.existsById(id);
+		
+		if(existe) 
+		{
+			repository.deleteById(id);
+			return ResponseEntity.ok().build();
+		} 
+		else
+		{
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@PostMapping
+	public ResponseEntity<Gasto> inserir(@RequestBody Gasto obj)
+	{
+		repository.save(obj);
+		return ResponseEntity.ok(obj);
 	}
 }
